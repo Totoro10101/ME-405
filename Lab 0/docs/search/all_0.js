77a2a21ece870c75a02984578f4ce1d62bd882ac
Totoro10101 <67016573+Totoro10101@users.noreply.github.com>
2022-01-23 10:34:20 -0800

    Revert "deleted docs"
    This reverts commit 3e0aa04c3872d001f3f470b275b9d5e3fabf67dd.

---
+++ Lab 0/docs/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['led_5fbrightness_0',['led_brightness',['../main_8py.html#a3179b43e0aed4265bba202bccca8df62',1,'main']]],
+  ['led_5fsetup_1',['led_setup',['../main_8py.html#a4f9b44ab4bc5a5c945aa2c8d9096bcaf',1,'main']]]
+];
